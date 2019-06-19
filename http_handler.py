f46f757c7c9711038d559cd6d190795dfa970a75
v5yangzai <84328409@qq.com>
2019-06-19 16:36:46 +0800

    First Commit

---
+++ http_handler.py
@@ -0,0 +1,72 @@
+import re
+import aiohttp
+from yarl import URL
+from typing import Union, Optional
+from asyncio import AbstractEventLoop
+from aiologger.levels import LogLevel
+from aiologger.formatters.base import Formatter
+from aiologger.filters import Filter
+from aiologger.handlers.base import Handler
+from aiologger.records import LogRecord
+
+
+StrOrURL = Union[str, URL]
+
+
+def _get_msg_dict(self, record):
+    # self.format(record)
+    record.message = record.get_message()
+    record.asctime = self.format_time(record, self.datefmt)
+    params = re.findall("%\((.*?)\)", self._fmt)
+    _d = {}
+    for i in params:
+        _v = record.__dict__.get(i)
+        if isinstance(_v, LogLevel):
+            _v = _v.value
+        _d[i] = str(_v) if _v else ''
+    return _d
+
+
+class AsyncHTTPHandler(Handler):
+
+    def __init__(self,
+                 url: StrOrURL,
+                 method: str="GET",
+                 level: Union[str, int, LogLevel] = LogLevel.NOTSET,
+                 formatter: Formatter = None,
+                 filter: Filter = None,
+                 *,
+                 loop: Optional[AbstractEventLoop] = None,
+                 ):
+        super().__init__(loop=loop)
+        self._url = url
+        self._method = method.upper()
+        self.level = level
+        self.formatter: Formatter = formatter if formatter else Formatter()
+        self.formatter.__class__.get_msg_dict = _get_msg_dict
+        # self.formatter.get_msg_dict = _get_msg_dict  # add get_msg_dict for formatter
+
+        self.sesison = aiohttp.ClientSession()
+        if filter:
+            self.add_filter(filter)
+
+    @property
+    def initialized(self):
+        return True
+
+    async def emit(self, record: LogRecord):
+        func = getattr(self.formatter, "get_msg_dict")
+        data = func(record)
+        kwargs = {
+            "url": self._url,
+            "method": self._method
+        }
+        if self._method == "POST":
+            kwargs["data"] = data
+        else:
+            kwargs["params"] = data
+
+        await self.sesison.request(**kwargs)
+
+    async def close(self):
+        await self.sesison.close()
